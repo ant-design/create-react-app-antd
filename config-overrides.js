8f72f965b87744d0e9766b86fc3dc52a23af2d6e
Andreas Cederström <andriijas@users.noreply.github.com>
2017-11-01 12:37:21 +0100

    Upgrade packages and config

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,8 @@ const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-  config = rewireLess(config, env, {
+  config = rewireLess.withLoaderOptions({
     modifyVars: { "@primary-color": "#1DA57A" },
-  });
+  })(config, env);
   return config;
 };
