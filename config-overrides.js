fdc20ed524878cd20cbb4325306c742295f9ad05
vadistic <vadistic@gmail.com>
2018-04-29 01:31:47 +0200

    upgrade

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,13 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const { injectBabelPlugin } = require("react-app-rewired");
+const rewireLess = require("react-app-rewire-less");
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", style: true }],
+    config
+  );
   config = rewireLess.withLoaderOptions({
-    modifyVars: { "@primary-color": "#1DA57A" },
+    modifyVars: { "@primary-color": "black" }
   })(config, env);
   return config;
 };
