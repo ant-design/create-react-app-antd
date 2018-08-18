8c43c26175ae25a28ba3b96bfba4f4215667d0c6
afc163 <afc163@gmail.com>
2018-08-18 23:34:55 +0800

    sync source with newest cra version

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
   config = rewireLess.withLoaderOptions({
     modifyVars: { "@primary-color": "#1DA57A" },
-    javascriptEnabled: true 
+    javascriptEnabled: true,
   })(config, env);
   return config;
 };
