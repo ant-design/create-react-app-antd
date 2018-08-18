3e5efb860034e1d20999e13274d9dbb96048c5a9
afc163 <afc163@gmail.com>
2018-08-18 23:41:30 +0800

    libraryDirectory => "es"

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,10 @@ const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // change importing css to less
+    config,
+  );
   config = rewireLess.withLoaderOptions({
     modifyVars: { "@primary-color": "#1DA57A" },
     javascriptEnabled: true,
