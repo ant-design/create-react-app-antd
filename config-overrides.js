948f6d656f954095ce801e5fbc5947e7b7159bca
afc163 <afc163@gmail.com>
2019-02-19 01:16:54 +0800

    ✨ implement a antd form and add package-lock.json

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,6 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#1DA57A' },
+    modifyVars: { '@primary-color': '#25b864' },
   }),
 );
